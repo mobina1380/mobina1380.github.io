4095906032d6034a86f45b31b2b893f9a196464f
mobina1380 <esmaeilimobina98@gmail.com>
2024-09-15 09:57:42 -0700

    fix version2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 
 import {FC, memo} from 'react';
 
-
+import Header from '../components/Header'
+import About from '../components/About'
+import Experience from '../components/Experience'
+import Education from '../components/Education'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Certifications from '../components/Certifications'
+import Footer from '../components/Footer'
 
 
 const Home: FC = memo(() => {
 
   return (
-   <>
-   salam
-   </>
+    <div className="bg-gray-100 min-h-screen">
+    {/* <Header />
+    <main className="container mx-auto px-4 py-8">
+      <About />
+      
+      <Education />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Certifications />
+    </main>
+    <Footer /> */}
+  </div>
   );
 });
 
