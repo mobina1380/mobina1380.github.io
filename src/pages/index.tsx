f1948b1d593fc44df144466efc09ae7a9600ef4b
mobina1380 <esmaeilimobina98@gmail.com>
2024-09-15 10:00:04 -0700

    fix version2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,6 @@
 
 import {FC, memo} from 'react';
 
-import Header from '../components/Header'
-import About from '../components/About'
-import Experience from '../components/Experience'
-import Education from '../components/Education'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Certifications from '../components/Certifications'
-import Footer from '../components/Footer'
-
 
 const Home: FC = memo(() => {
 
