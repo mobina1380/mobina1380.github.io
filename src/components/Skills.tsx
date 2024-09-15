aa82bd6e215bbed53e2ce4be733c1447226e8d37
mobina1380 <esmaeilimobina98@gmail.com>
2024-09-15 10:03:25 -0700

    fix version2

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,48 +1,48 @@
 export default function Skills() {
-    const skillCategories = [
-      {
-        category: "Artificial Intelligence & Machine Learning",
-        skills: [
-          "Automatic Speech Recognition (ASR)",
-          "Speech Emotion Recognition (SER)",
-          "Natural Language Processing (NLP)",
-          "Deep Learning (CNNs, RNNs, LSTMs)",
-          "Voice Activity Detection (VAD)",
-          "Model Evaluation and Tuning",
-          "Machine Learning Algorithms (SVM, KNN, Decision Trees)",
-          "Deep Learning for Recommender Systems",
-          "User Behavior Analysis",
-          "Personalization Techniques",
-          "Scalability and Performance Optimization",
-          "Mathematics, Linear algebra, and Statistics"
-        ]
-      },
-      {
-        category: "Programming Languages",
-        skills: ["Python", "JavaScript", "TypeScript"]
-      },
-      {
-        category: "Database",
-        skills: ["MySQL"]
-      },
-      {
-        category: "Libraries and Frameworks",
-        skills: ["Pandas", "Scikit-Learn", "TensorFlow", "PyTorch"]
-      },
-      {
-        category: "Tools",
-        skills: ["Git", "Docker", "Airflow"]
-      },
-      {
-        category: "Development Methodologies",
-        skills: ["Agile Scrum"]
-      }
-    ]
+    // const skillCategories = [
+    //   {
+    //     category: "Artificial Intelligence & Machine Learning",
+    //     skills: [
+    //       "Automatic Speech Recognition (ASR)",
+    //       "Speech Emotion Recognition (SER)",
+    //       "Natural Language Processing (NLP)",
+    //       "Deep Learning (CNNs, RNNs, LSTMs)",
+    //       "Voice Activity Detection (VAD)",
+    //       "Model Evaluation and Tuning",
+    //       "Machine Learning Algorithms (SVM, KNN, Decision Trees)",
+    //       "Deep Learning for Recommender Systems",
+    //       "User Behavior Analysis",
+    //       "Personalization Techniques",
+    //       "Scalability and Performance Optimization",
+    //       "Mathematics, Linear algebra, and Statistics"
+    //     ]
+    //   },
+    //   {
+    //     category: "Programming Languages",
+    //     skills: ["Python", "JavaScript", "TypeScript"]
+    //   },
+    //   {
+    //     category: "Database",
+    //     skills: ["MySQL"]
+    //   },
+    //   {
+    //     category: "Libraries and Frameworks",
+    //     skills: ["Pandas", "Scikit-Learn", "TensorFlow", "PyTorch"]
+    //   },
+    //   {
+    //     category: "Tools",
+    //     skills: ["Git", "Docker", "Airflow"]
+    //   },
+    //   {
+    //     category: "Development Methodologies",
+    //     skills: ["Agile Scrum"]
+    //   }
+    // ]
   
     return (
       <section className="mb-8">
         <h2 className="text-2xl font-bold mb-4">Skills</h2>
-        {skillCategories.map((category, index) => (
+        {/* {skillCategories.map((category, index) => (
           <div key={index} className="mb-4">
             <h3 className="text-xl font-semibold mb-2">{category.category}</h3>
             <ul className="list-disc list-inside grid grid-cols-2 gap-2">
@@ -51,7 +51,7 @@ export default function Skills() {
               ))}
             </ul>
           </div>
-        ))}
+        ))} */}
       </section>
     )
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 
 import {FC, memo} from 'react';
 
+import Header from '../components/Header'
+import About from '../components/About'
+import Experience from '../components/Experience'
+import Education from '../components/Education'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Certifications from '../components/Certifications'
+import Footer from '../components/Footer'
+
 
 const Home: FC = memo(() => {
 
   return (
     <div className="bg-gray-100 min-h-screen">
-    {/* <Header />
+    <Header />
     <main className="container mx-auto px-4 py-8">
       <About />
       
@@ -16,7 +25,7 @@ const Home: FC = memo(() => {
       <Projects />
       <Certifications />
     </main>
-    <Footer /> */}
+    <Footer />
   </div>
   );
 });
